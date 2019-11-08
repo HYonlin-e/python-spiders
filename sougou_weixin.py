3b1affd95e5ff5913bbbebdc81dfc812e49da06b
hyonline <hyonline95@126.com>
2019-11-08 09:46:40 +0800

    v1

--- sougou_weixin.py
+++ sougou_weixin.py
@@ -9,9 +9,9 @@ import random
 import json
 
 
-def main():
+def main(UserAgent):
     # 列表页拿到itemurls和thumbs,但是itemurls不能直接用
-    list_url = 'https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E6%9F%AC%E5%9F%94%E5%AF%A8&ie=utf8&_sug_=n&_sug_type_=&page=1'
+    list_url = 'https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E6%9F%AC%E5%9F%94%E5%AF%A8&ie=utf8&_sug_=n&_sug_type_='
     headers1 = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
         "Accept-Encoding": "gzip, deflate, br",
@@ -19,13 +19,13 @@ def main():
         "Connection": "keep-alive",
         "Host": "weixin.sogou.com",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
+        "User-Agent": UserAgent,
     }
     response1 = requests.get(list_url, headers=headers1)
     html = etree.HTML(response1.text)
     urls = ['https://weixin.sogou.com' + i for i in html.xpath('//div[@class="img-box"]/a/@href')]
 
-    params = get_cookie(response1)
+    params = get_cookie(response1, UserAgent)
     url = get_k_h(urls[0])
     headers2 = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -42,10 +42,9 @@ def main():
         "Host": "weixin.sogou.com",
         "Referer": list_url,
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"
+        "User-Agent": UserAgent
     }
     response2 = requests.get(url, headers=headers2)
-    print(response2.status_code)
 
     fragments = re.findall("url \+= '(.*?)'", response2.text, re.S)
     itemurl = ''
@@ -53,21 +52,18 @@ def main():
         itemurl += i
 
     # 文章url拿正文
-    # url = "https://mp.weixin.qq.com/s?src=11&timestamp=1572921220&ver=1955&signature=McZREDXqOED*1xGVkhuFXpNF7KcV54DRRSYjyPbwZvSO20QRO77a3w9RjoHAKxMkyUJkWaE9llLPp8zIF97qqCrolVg-gQZgeXof3Sisb0rAJ6848aBzVULDOJZZpdKb&new=1"
     headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
         "cache-control": "max-age=0",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+        "user-agent": UserAgent
     }
     response3 = requests.get(itemurl, headers=headers)
-    html = etree.HTML(response3.text)
     print(response3.status_code)
-    print(html.xpath('//meta[@property="og:title"]/@content')[0])
 
 
-def get_cookie(response1):
+def get_cookie(response1, UserAgent):
     SetCookie = response1.headers['Set-Cookie']
     ABTEST = re.findall('ABTEST=(.*?);', SetCookie, re.S)[0]
     SNUID = re.findall('SNUID=(.*?);', SetCookie, re.S)[0]
@@ -83,7 +79,7 @@ def get_cookie(response1):
         "Cookie": "SNUID={}; IPLOC={}".format(SNUID, IPLOC),
         "Host": "www.sogou.com",
         "Referer": "https://weixin.sogou.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+        "User-Agent": UserAgent
     }
     response2 = requests.get(url, headers=headers)
     SetCookie = response2.headers['Set-Cookie']
@@ -97,8 +93,8 @@ def get_cookie(response1):
         "Connection": "keep-alive",
         "Cookie": "ABTEST={}; SNUID={}; IPLOC={}; SUID={}".format(ABTEST, SNUID, IPLOC, SUID1),
         "Host": "weixin.sogou.com",
-        "Referer": "https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E6%9F%AC%E5%9F%94%E5%AF%A8&ie=utf8&_sug_=n&_sug_type_=",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+        "Referer": response1.url,
+        "User-Agent": UserAgent
     }
     response3 = requests.get(url, headers=headers)
     SetCookie = response3.headers['Set-Cookie']
@@ -114,7 +110,7 @@ def get_cookie(response1):
         "Cookie": "SNUID={}; IPLOC={}; SUID={}".format(SNUID, IPLOC, SUID2),
         "Host": "pb.sogou.com",
         "Referer": "https://weixin.sogou.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+        "User-Agent": UserAgent
     }
     response4 = requests.get(url, headers=headers, params=uigs_para)
     SetCookie = response4.headers['Set-Cookie']
@@ -149,7 +145,3 @@ def get_uigs_para(response):
     uigs_para['right'] = 'right0_0'
     uigs_para['exp_id'] = exp_id[:-1]
     return uigs_para
-
-
-if __name__ == "__main__":
-    main()
